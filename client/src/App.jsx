05c8cb278b3c152aa993441c4e7bbcd41e9ea20e
hello <hello@gmail.com>
2024-08-02 16:17:22 +0530

    Eco Corp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import viteLogo from '/vite.svg'
-import './App.css'
+import { Donate } from './Components/Donate'
+import DonateForms from './Components/DonateForms'
+import DonateActivities from './Components/DonateActivities'
+import { EcoCorpHero } from './Components/EcoCorpHero'
+import { Page1 } from './Components/EcoCsrp/Page1'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div>
-      <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
-    </div>
+<Page1/>
   )
 }
 
