c3cdd29f699eb863062eb522b4223534ccd64c39
hello <hello@gmail.com>
2024-08-05 12:25:31 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,17 @@ import Feature from './Components/Feature'
 import Footer from './Components/Footer'
 import ContactSection from './Components/Contact'
 import Hero from './Components/Hero'
-
+import DonateForms from './Components/DonateForms'
+import Donate from '../src/Components/Donate.jsx';
+import Counter from './Components/Counter.jsx';
+import { Page2 } from './Components/EcoCsrp/Page2.jsx'
+import { Page3 } from './Components/EcoCsrp/Page3.jsx'
+import Form from './Components/EcoCsrp/EcoCorpForm.jsx'
+import { EcoCorpHero } from './Components/EcoCorpHero.jsx'
 const App = () => {
   return (
     <div>
+      <EcoCorpHero/>
     </div>
   )
 }
