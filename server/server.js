abd09c2bc32b7637e39660a83aa7ea0aa4841d6b
santhoshkrishnaks <santhoshkrishna771@gmail.com>
2024-08-08 13:55:32 +0530

    Server Deployment

--- server/server.js
+++ server/server.js
@@ -1,32 +1,41 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const dotenv=require('dotenv');
-const {Webhook}=require('svix');
-const bodyParser=require('body-parser');
-const user=require('./model/model.user.js');
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const { Webhook } = require('svix');
+const bodyParser = require('body-parser');
+const user = require('./model/model.user.js');
+const cors = require('cors');
+
 dotenv.config();
-const cors=require('cors');
-const app= express();
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect("mongodb+srv://admin:Ecovate@ecovate.5mgaa.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Ecovate").then(() => {
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
     console.log("Connected to mongoose");
     app.listen(5000, () => {
       console.log("NodeApi app is running on localhost 5000");
     });
   })
   .catch((error) => {
-    console.log(error);
+    console.error("Database connection error:", error);
   });
-app.get('/',(req,res)=>{
+
+app.get('/', (req, res) => {
   res.send("Hello World");
 });
+
 app.post(
   '/api/webhooks',
   bodyParser.raw({ type: 'application/json' }),
-  async function (req, res) {
+  async (req, res) => {
     try {
+      console.log("Webhook received");
+
       const payloadString = req.body.toString();
       const svixHeaders = req.headers;
 
@@ -43,6 +52,7 @@ app.post(
         const email = attributes.email_addresses[0].email_address;
         const profile_img_url = attributes.profile_image_url;
         const username = attributes.username;
+
         const User = new user({
           email_address: email,
           first_name: firstName,
@@ -50,20 +60,29 @@ app.post(
           user_name: username,
           profile_img_url: profile_img_url
         });
+        
         await User.save();
         console.log('User is created');
       }
 
       res.status(200).json({
         success: true,
-        message: 'Webhook received',
+        message: 'Webhook processed successfully',
       });
     } catch (err) {
-      console.error(err);
+      console.error("Webhook processing error:", err);
       res.status(400).json({
         success: false,
-        message: err.message,
+        message: 'Error processing webhook: ' + err.message,
       });
     }
   }
 );
+
+// Catch-all for 404 errors
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
