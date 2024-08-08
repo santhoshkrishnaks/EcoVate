c48e791fd3cf57a4275428ea584930a17fe987b4
santhoshkrishnaks <santhoshkrishna771@gmail.com>
2024-08-08 13:34:38 +0530

    Server Deployment

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const bodyParser=require('body-parser');
 const user=require('./model/model.user.js');
 dotenv.config();
 const cors=require('cors');
+const app= express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://admin:Ecovate@ecovate.5mgaa.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Ecovate").then(() => {
     console.log("Connected to mongoose");
     app.listen(5000, () => {
@@ -15,10 +19,6 @@ mongoose.connect("mongodb+srv://admin:Ecovate@ecovate.5mgaa.mongodb.net/Node-API
   .catch((error) => {
     console.log(error);
   });
-const app= express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.get('/',(req,res)=>{
   res.send("Hello World");
 });
@@ -40,15 +40,15 @@ app.post(
       if (eventType === 'user.created') {
         const firstName = attributes.first_name;
         const lastName = attributes.last_name;
-        const email=attributes.email_addresses[0].email_address;
-        const profile_img_url=attributes.profile_image_url;
-        const username=attributes.username;
+        const email = attributes.email_addresses[0].email_address;
+        const profile_img_url = attributes.profile_image_url;
+        const username = attributes.username;
         const User = new user({
-          email_address:email,
-          first_name:firstName,
-          last_name:lastName,
-          user_name:username,
-          profile_img_url:profile_img_url
+          email_address: email,
+          first_name: firstName,
+          last_name: lastName,
+          user_name: username,
+          profile_img_url: profile_img_url
         });
         await User.save();
         console.log('User is created');
@@ -59,6 +59,7 @@ app.post(
         message: 'Webhook received',
       });
     } catch (err) {
+      console.error(err);
       res.status(400).json({
         success: false,
         message: err.message,
