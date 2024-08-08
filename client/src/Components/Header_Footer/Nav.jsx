896eae63bdd9cd32745ff112daeae70c3dd02119
vimal1603 <vimalc162020@gmail.com>
2024-08-08 06:28:39 +0530

    link

--- client/src/Components/Header_Footer/Nav.jsx
+++ client/src/Components/Header_Footer/Nav.jsx
@@ -25,9 +25,9 @@ const Nav = () => {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 lg:-ml-10 ">
+          <Link to="/" className="-m-1.5 lg:-ml-10 ">
             <img className="h-[68px] w-auto" src={logo} alt="" />
-          </a>
+          </Link>
         </div>
 
         <div className="flex lg:hidden">
@@ -71,9 +71,9 @@ const Nav = () => {
           <span
             onClick={() => {
               if (isSignedIn) {
-                window.location.href = "/Ecofund";
+                navi("/Ecofund");
               } else {
-                window.location.href = "/signin";
+                navi("/signin");
               }
             }}
             className="text-lg font-semibold leading-6 text-gray-900 cursor-pointer"
@@ -83,9 +83,9 @@ const Nav = () => {
           <span
             onClick={() => {
               if (isSignedIn) {
-                window.location.href = "/EcoCorp";
+                navi("/EcoCorp");
               } else {
-                window.location.href = "/signin";
+                navi("/signin");
               }
             }}
             className="text-lg font-semibold leading-6 text-gray-900 cursor-pointer"
@@ -164,9 +164,9 @@ const Nav = () => {
           <div className="fixed inset-0 z-10"></div>
           <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-green-100 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="/" className="-m-1.5 p-1.5">
+              <Link to="/" className="-m-1.5 p-1.5">
                 <img className="h-8 w-auto" src={logo} alt="" />
-              </a>
+              </Link>
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -192,36 +192,36 @@ const Nav = () => {
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6 text-lg">
-                  <a
-                    href="/Ecoconnect"
+                  <Link
+                    to="/Ecoconnect"
                     className="-mx-3 block rounded-lg py-2 px-3 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
                   >
                     EcoConnect
-                  </a>
-                  <a
-                    href="/Ecofund"
+                  </Link>
+                  <Link
+                    to="/Ecofund"
                     className="-mx-3 block rounded-lg py-2 px-3 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
                   >
                     EcoFund
-                  </a>
-                  <a
-                    href="/Ecocorp"
+                  </Link>
+                  <Link
+                    to="/Ecocorp"
                     className="-mx-3 block rounded-lg py-2 px-3 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
                   >
                     EcoCorp
-                  </a>
+                  </Link>
                   <Link
                     to="/Ecovision"
                     className="-mx-3 block rounded-lg py-2 px-3 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
                   >
                     EcoVision
                   </Link>
-                  <a
-                    href="/Ecocalc"
+                  <Link
+                    to="/Ecocalc"
                     className="-mx-3 block rounded-lg py-2 px-3 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
                   >
                     EcoCalc
-                  </a>
+                  </Link>
                 </div>
                 {isSignedIn ? (
                   <div></div>
