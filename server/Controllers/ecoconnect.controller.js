aab5786b34a588cb988aef528800db122e397a4e
hello <hello@gmail.com>
2024-08-11 18:43:28 +0530

    controller added

--- server/Controllers/ecoconnect.controller.js
+++ server/Controllers/ecoconnect.controller.js
@@ -1,62 +1,59 @@
-import { EcoConnect } from "../config/database";
+import { EcoConnect } from "../config/database.js";
 
 //create post
 
-const createPost=async (req,res)=>{
-
-  try{
-    const posst=await new EcoConnect(req.body);
+export const createPost = async (req, res) => {
+  try {
+    const posst = await new EcoConnect(req.body);
     posst.save();
     res.status(200).json(posst);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-  catch(error){
-    res.status(400).json({error:error.message})
-  }
-}
+};
 
 //get post all
 
-const getPostAll= async(req,res)=>{
-  try{
-    const posstAll=await EcoConnect.find({});
+export const getPostAll = async (req, res) => {
+  try {
+    const posstAll = await EcoConnect.find({});
     res.status(200).json(posstAll);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-  catch(error)
-  {
-    res.status(400).json({error:error.message});
-  }
-}
-
-
+};
 
 //get post by username
 
-const getPostbyUser =async(req,res)=>{
-  try{
-    const username = req.body;
-    const posstuser=await EcoConnect.findbyuser({username});
-  if(!posstuser)
-  {
-    return res.status(404).json("user not found")
-  }
-  res.status(200).json(posstuser);
-  }
-  catch(error){
+export const getPostbyUser = async (req, res) => {
+  try {
+    const {username} = req.params;
+    const posstuser = await EcoConnect.findbyuser(username);
+    console.log(req.body);
+    if (!posstuser ) {
+      return res.status(404).json("user not found");
+    }
+    res.status(200).json(posstuser);
+  } catch (error) {
     console.log(error.message);
-    res.status(500).json({message:error.message});
+    res.status(500).json({ message: error.message });
   }
-}
-
+};
 
 // delete post
 
-const deletebyId=async(req,res)=>{
-  
-
-}
-
-
-
-
-
-
+export const deletebyId = async (req, res) => {
+  try {
+    const {title} = req.params;
+    const deletepost = await EcoConnect.findOneAndDelete({
+      title: new RegExp(`^${title}$`, "i"),
+    });
+    if (!deletepost) {
+      return res.status(404).json({ message: "post not found" });
+    }
+    res.status(200).json({ message: "post deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
--- server/routes/ecoconnect.route.js
+++ server/routes/ecoconnect.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-
-import { createPost, getPosts } from "../Controllers/ecoconnect.controller.js";
+import {
+  createPost,
+  deletebyId,
+  getPostAll,
+  getPostbyUser,
+} from "../Controllers/ecoconnect.controller.js";
 const router = express.Router();
 
 router.post("/posts", createPost);
-router.post("/posts", getPosts);
+router.get("/posts", getPostAll);
+router.get("/posts/:username", getPostbyUser);
+router.delete("/posts/:title", deletebyId);
 
 export default router;
