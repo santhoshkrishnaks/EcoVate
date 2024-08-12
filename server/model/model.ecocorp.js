bb7b65546cd64bb4b2b2649f1a5549c22c40ba23
VR22124 <rohith.skct.ece@gmail.com>
2024-08-12 22:27:28 +0530

    ecocorp

--- server/model/model.ecocorp.js
+++ server/model/model.ecocorp.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const ecocorpSchema=mongoose.Schema({
+    email_address:{
+        type:String,
+        required:true,
+    },
     org_name:{
         type:String,
         required:true
