b8e18b5ebcee454bc652299a4246956099d428c7
hello <hello@gmail.com>
2024-08-12 13:54:12 +0530

    all controllers

--- server/server.js
+++ server/server.js
@@ -5,38 +5,28 @@ import { connectdb } from "./config/database.js";
 import { landController } from "./Controllers/landing.controller.js";
 import corprouter from "./Routes/route.ecocorp.js";
 import cors from "cors";
-<<<<<<< HEAD
-import router from "./routes/ecoconnect.route.js";
-import newsroute from '../server/routes/route.econews.js';
-=======
-import router from "./Routes/route.ecoconnect.js";
+import router from "./routes/route.ecoconnect.js";
 import fundrouter from "./Routes/route.ecofund.js";
-import calcRouter from "./Routes/route.ecocalc.js"
+import newsrouter from './routes/route.econews.js'
+import calcRouter from "../server/routes/route.ecocalc.js";
 import ecovisionrouter from "./Routes/route.ecovision.js";
 import volunteerrouter from "./Routes/route.volunteer.js";
 
-
->>>>>>> e9036aa99915c8f6682f9f69451d3a2d2dafd5ec
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 connectdb();
 app.use(cors());
-<<<<<<< HEAD
-app.use('/',router)
-app.use('/',newsroute)
-=======
 
-
-app.use('/',router);
-app.use('/',ecovisionrouter);
-app.use('/',volunteerrouter);
->>>>>>> e9036aa99915c8f6682f9f69451d3a2d2dafd5ec
+app.use("/", router);
+app.use("/", ecovisionrouter);
+app.use("/", volunteerrouter);
+app.use("/", newsrouter);
 app.get("/", landController);
 app.post("/api/webhook", bodyParser.raw({ type: "application/json" }), login);
-app.use("/",corprouter);
-app.use("/",fundrouter);
-app.use("/",calcRouter)
+app.use("/", corprouter);
+app.use("/", fundrouter);
+app.use("/", calcRouter);
 app.listen(5000, () => {
   console.log(`listening on port 5000`);
 });
