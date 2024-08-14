b86ec9b1938b3cb46b1ce2ecd175466b4d89a318
Santhosh Krishna K S <147795357+santhoshkrishnaks@users.noreply.github.com>
2024-08-14 14:09:23 +0530

    Update FundData.jsx

--- client/src/Components/Admin/FundData.jsx
+++ client/src/Components/Admin/FundData.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useUser } from "@clerk/clerk-react";
 
 const FundPage = () => {
   const [payments, setPayments] = useState([]);
