8484dbf7fb43618637a4b7d42743bad51e615a7b
santhoshkrishnaks <santhoshkrishna771@gmail.com>
2024-11-15 17:58:39 +0530

    Clerk Update

--- server/Controllers/user.controller.js
+++ server/Controllers/user.controller.js
@@ -2,51 +2,38 @@ import { User, connectdb } from "../config/database.js";
 import { Webhook } from "svix";
 import dotenv from "dotenv";
 dotenv.config();
+
 export const login = async (req, res) => {
   try {
-    const payloadString = req.body.toString();
+    const payloadString = JSON.stringify(req.body); // Ensure correct format
 
-    const svixHeaders = req.headers;
+    const svixHeaders = req.headers; // Pass headers as received
 
     const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET_KEY);
 
     const evt = wh.verify(payloadString, svixHeaders);
 
     const { id, ...attributes } = evt.data;
-
     const eventType = evt.type;
 
     if (eventType === "user.created") {
-      let firstName = attributes.first_name;
-
-      let lastName = attributes.last_name;
-
-      let email = attributes.email_addresses[0].email_address;
-
-      let img = attributes.profile_image_url;
-
-      let usernam = attributes.username;
-
       const user = new User({
         clerkUserId: id,
-        firstName: firstName,
-        lastName: lastName,
-        userName: usernam,
-        profileImg: img,
-        emailAddress: email,
+        firstName: attributes.first_name,
+        lastName: attributes.last_name,
+        userName: attributes.username,
+        profileImg: attributes.profile_image_url,
+        emailAddress: attributes.email_addresses[0].email_address,
       });
-      try {
-        await user.save();
-        console.log("User is created");
-      } catch (err) {
-        console.error("Error saving user:", err.message);
-      }
+      await user.save();
+      console.log("User created:", user);
     } else if (eventType === "user.deleted") {
-      const id1 = await User.findOneAndDelete({ clerkUserId: id });
-      if (!id1) {
+      const user = await User.findOneAndDelete({ clerkUserId: id });
+      if (!user) {
+        console.error("User not found for deletion");
         return res.status(500).json({ message: "User not found" });
       }
-      console.log("delete");
+      console.log("User deleted:", user);
     } else if (eventType === "user.updated") {
       const updateData = {
         firstName: attributes.first_name,
@@ -55,36 +42,43 @@ export const login = async (req, res) => {
         profileImg: attributes.profile_image_url,
         emailAddress: attributes.email_addresses[0].email_address,
       };
-      const id1 = await User.findOneAndUpdate({ clerkUserId: id }, updateData, {
-        new: true,
-        runValidators: true,
-      });
-      if (!id1) {
+      const user = await User.findOneAndUpdate(
+        { clerkUserId: id },
+        updateData,
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+      if (!user) {
+        console.error("User not found for update");
         return res.status(500).json({ message: "User not found" });
       }
-      console.log("Updated");
+      console.log("User updated:", user);
     }
+
     res.status(200).json({
       success: true,
       message: "Webhook received",
     });
   } catch (err) {
+    console.error("Error processing webhook:", err.message);
     res.status(400).json({
       success: false,
       message: err.message,
     });
   }
 };
-export const getuser=async (req,res)=>{
-  try{
-    const {username}=req.params;
-    const user=await User.findOne({ userName: new RegExp(username, "i") });
-    if(!user){
+
+export const getuser = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findOne({ userName: new RegExp(username, "i") });
+    if (!user) {
       res.status(404).json("Not Found");
     }
     res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-  catch(error){
-    res.status(400).json({error:error.message});
-  }
-}
+};
--- server/server.js
+++ server/server.js
@@ -33,6 +33,6 @@ app.use("/", commentrouter);
 app.use("/", likerouter);
 
 
-app.listen(3000, () => {
+app.listen(5000, () => {
   console.log(`listening on port 5000`);
 });
